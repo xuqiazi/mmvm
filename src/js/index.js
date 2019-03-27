08f6718b1fa16b20d776fc4d6b0ddfe8fc909f3f
qiazi <leisiqi@leisiqideMacBook-Pro.local>
2019-03-27 09:56:49 +0800

    codes

---
+++ src/js/index.js
@@ -0,0 +1,117 @@
+let uid = 0;
+class Dep {
+    constructor() {
+        this.id = uid++;
+        this.subs = [];
+        this.target = null;
+    }
+    addSub(sub) { 
+        this.subs.push(sub);
+    }
+    notify() { 
+        this.subs.forEach(sub => sub.update());
+    }
+}
+class observer {
+    constructor(value) {
+        this.value = value;
+        Object.keys(value).forEach(key => defineReactive(this.value, key, value[key]))
+    }
+}
+
+function observe(value) {
+    if (!value || typeof value !== 'object') {
+        return;
+    }
+    return new observer(value);
+}
+
+function defineReactive(obj, key, val) {
+    const dep = new Dep();
+    let chlidObj = observe(val);
+    Object.defineProperty(obj, key, {
+        get: () => {
+            if (Dep.target) {
+                Dep.target.addDep(dep);
+            }
+            return val;
+        },
+        set: newVal => {
+            if (val === newVal) return;
+            val = newVal;
+            chlidObj = observe(newVal);
+            dep.notify();
+        }
+    });
+}
+class watcher {
+    constructor(vm, data, obj) {
+        this.depIds = {};
+        this.vm = vm;
+        this.data = data;
+        this.obj = obj;
+        this.val = this.get();
+    }
+    addDep(dep) {
+        if (!this.depIds.hasOwnProperty(dep.id)) {
+            dep.addSub(this);
+            this.depIds[dep.id] = dep;
+        }
+    }
+    update() {
+        const val = this.get();
+        if (val !== this.val){
+            this.val = val;
+            this.obj.call(this.vm, val);
+        }
+    }
+    get() {
+        Dep.target = this;
+        const val = this.vm._data[this.data];
+        Dep.target = null;
+        return val;
+    }
+}
+class Vue {
+    constructor(options = {}) {
+        this.$options = options;
+        let data = (this._data = this.$options.data);
+        Object.keys(data).forEach(key => this._proxy(key));
+        observe(data);
+    }
+    $watch(data, obj) {
+        new watcher(this, data, obj);
+    }
+    _proxy(key) {
+        Object.defineProperty(this, key, {
+            get: () => this._data[key],
+            set: val => {
+                this._data[key] = val;
+            }
+        })
+    }
+}
+
+let demo1 = new Vue({
+    data: {
+        text: '',
+    },
+});
+let demo2 = new Vue({
+    data: {
+        text: '',
+    }
+});
+const p = document.getElementById('p');
+const input = document.getElementById('input');
+const hello = document.getElementById('test');
+input.addEventListener('keyup', function (e) {
+    demo1.text = e.target.value;
+});
+demo1.$watch('text', str => p.innerHTML = str);
+document.addEventListener('DOMContentLoaded', function () {
+    demo2.$watch('text', str => hello.innerHTML = str);
+    setTimeout(() => {
+        demo2.$options.data.text = `it's changed...`
+    }, 3000);
+}, false)
---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const px2viewport = require('postcss-plugin-pxtoviewport');
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production'
+    return {
+        entry: [
+            "babel-polyfill",
+            path.join(__dirname, './src/js/index.js')
+        ],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/bundle.js'
+        },
+        devServer: {
+            port: 8080, //端口号
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: require.resolve('zepto'),
+                    loader: 'exports-loader?window.Zepto!script-loader'
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: require.resolve('postcss-loader'),
+                            options: {
+                                ident: 'postcss',
+                                plugins: () => [
+                                    px2viewport({
+                                        viewportWidth: 1125,
+                                        toViewport: true
+                                    })
+                                ],
+                            }
+                        },
+                        'sass-loader',
+                    ]
+                },
+                {
+                    test: /\.html$/,
+                    use: [{
+                        loader: "html-loader",
+                        options: {
+                            attrs: ['img:src', 'img:data-src', 'audio:src'],
+                            minimize: true
+                        }
+                    }]
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {}
+                    }]
+                }
+            ]
+        },
+        plugins: [
+            // ...,
+            new MiniCssExtractPlugin({
+                filename: "./css/[name].css",
+                chunkFilename: "./css/[id].css"
+            }),
+            new HtmlWebPackPlugin({
+                template: "./src/index.html",
+                filename: "./index.html",
+                minify: true,
+
+            }),
+            new CleanWebpackPlugin(['dist']),
+        ]
+    }
+}
