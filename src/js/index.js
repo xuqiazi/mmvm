b57790f9e26250e68cd0b799c6515a67d34c690a
qiazi <leisiqi@leisiqideMacBook-Pro.local>
2019-03-28 22:44:12 +0800

    compile

--- src/js/index.js
+++ src/js/index.js
@@ -17,6 +17,7 @@ class observer {
   constructor(value) {
     this.value = value;
     Object.keys(value).forEach(key => defineReactive(this.value, key, value[key]));
+    console.log(this.value);
   }
 }
 
@@ -40,17 +41,17 @@ function defineReactive(obj, key, val) {
     set: newVal => {
       if (val === newVal) return;
       val = newVal;
+      console.log(val);
       observe(newVal);
       dep.notify();
     },
   });
 }
 class watcher {
-  constructor(vm, data, obj) {
+  constructor(vm, data) {
     this.depIds = {};
     this.vm = vm;
     this.data = data;
-    this.obj = obj;
     this.val = this.get();
   }
   addDep(dep) {
@@ -63,64 +64,111 @@ class watcher {
     const val = this.get();
     if (val !== this.val) {
       this.val = val;
-      this.obj.call(this.vm, val);
+
     }
   }
   get() {
     Dep.target = this;
-    const val = this.vm._data[this.data];
+    const val = this.vm.$options.data[this.data];
     Dep.target = null;
     return val;
   }
 }
+const reg = /^\{{2}\S+\}{2}/;
 class Vue {
   constructor(options = {}) {
     this.$options = options;
-    const data = (this._data = this.$options.data);
-    Object.keys(data).forEach(key => this._proxy(key));
+    const data = this.$options.data;
+    const root = document.querySelector(this.$options.root);
+    const node = createNode(root, reg);
     observe(data);
+    compile(data, node);
+    Object.keys(data).forEach(key => {
+      this._proxy(key);
+      this.$Watch(key);
+      console.log(data, node);
+      compile(data, node);
+
+    });
+
   }
-  $Watch(data, obj) {
-    new watcher(this, data, obj);
+  $Watch(data) {
+    new watcher(this, data);
   }
   _proxy(key) {
     Object.defineProperty(this, key, {
-      get: () => this._data[key],
+      get: () => this.$options.data[key],
       set: val => {
-        this._data[key] = val;
+        this.$options.data[key] = val;
       },
     });
   }
 }
+// 需要把dom 上的节点拿到并且生成
 class dom {
-  constructor() {
-    this.key = key;
+  constructor(data, node) {
+    this.data = data;
+    this.node = node;
+    this.getNode(data, node);
   }
-  get() {
-
+  getNode(data, node) {
+    Object.keys(node).forEach(key => {
+      let str = node[key].innerHTML.substr(2);
+      str = str.substr(0, str.length - 2);
+      for (const prop in data) {
+        if (str === prop) {
+          node[key].innerHTML = data[prop];
+        }
+      }
+    });
   }
 }
 
-const demo1 = new Vue({
-  data: {
-    text: '',
-  },
-});
-const demo2 = new Vue({
-  data: {
-    text: '',
-  },
-});
-const p = document.getElementById('p');
-const input = document.getElementById('input');
-const hello = document.getElementById('test');
-input.addEventListener('keyup', function(e) {
-  demo1.text = e.target.value;
-});
-demo1.$Watch('text', str => (p.innerHTML = str));
+function compile(data, node) {
+  return new dom(data, node);
+}
+function createNode(root, reg) {
+
+  const nodeIterator = document.createNodeIterator(
+    root,
+    NodeFilter.SHOW_ELEMENT,
+    function(node) {
+      if (!node.children[0] && reg.test(node.textContent)) {
+        return NodeFilter.FILTER_ACCEPT;
+      }
+      return NodeFilter.FILTER_REJECT;
+    }
+  );
+  const pars = [];
+  let currentNode;
+  while ((currentNode = nodeIterator.nextNode())) {
+    pars.push(currentNode);
+  }
+  return pars;
+}
+// const root = document.querySelector('#app');
+
+
+// const p = document.getElementById('p');
+// const input = document.getElementById('input');
+// const hello = document.getElementById('test');
+// input.addEventListener('keyup', function(e) {
+//   demo1.text = e.target.value;
+// });
+// demo1.$Watch('text', str => (p.innerHTML = str));
+// document.addEventListener('DOMContentLoaded', function() {
+//   demo2.$Watch('text', str => (hello.innerHTML = str));
+//   setTimeout(() => {
+//     demo2.$options.data.text = 'it\'s changed...';
+//   }, 3000);
+// }, false);
 document.addEventListener('DOMContentLoaded', function() {
-  demo2.$Watch('text', str => (hello.innerHTML = str));
+  const opt = { root: '#app', data: { name: '检索中...', age: 30 } };
+  const vm = new Vue(opt);
+  // opt.data.name = 'dsdssd';
   setTimeout(() => {
-    demo2.$options.data.text = 'it\'s changed...';
-  }, 3000);
+    opt.data.name = '王永峰';
+    opt.data.age = 89;
+    // console.log(opt.data.name);
+  }, 2000);
 }, false);
