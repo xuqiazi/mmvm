52769384a9a99814838f73f240975c668e2690fe
qiazi <leisiqi@leisiqideMacBook-Pro.local>
2019-03-29 15:58:51 +0800

    add moudule

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,12 @@
 'use strict';
+
 let uid = 0;
+
+/**
+ *
+ *
+ * @class Dep
+ */
 class Dep {
   constructor() {
     this.id = uid++;
@@ -9,18 +16,29 @@ class Dep {
   addSub(sub) {
     this.subs.push(sub);
   }
-  notify() {
-    this.subs.forEach(sub => sub.update());
+  notify(key) {
+    this.subs.forEach(sub => sub.update(key));
   }
 }
+
+/**
+ *
+ *
+ * @class observer
+ */
 class observer {
   constructor(value) {
     this.value = value;
     Object.keys(value).forEach(key => defineReactive(this.value, key, value[key]));
-    console.log(this.value);
   }
 }
 
+/**
+ *
+ *
+ * @param {*} value
+ * @returns
+ */
 function observe(value) {
   if (!value || typeof value !== 'object') {
     return;
@@ -28,6 +46,13 @@ function observe(value) {
   return new observer(value);
 }
 
+/**
+ *
+ *
+ * @param {*} obj
+ * @param {*} key
+ * @param {*} val
+ */
 function defineReactive(obj, key, val) {
   const dep = new Dep();
   observe(val);
@@ -41,18 +66,23 @@ function defineReactive(obj, key, val) {
     set: newVal => {
       if (val === newVal) return;
       val = newVal;
-      console.log(val);
       observe(newVal);
-      dep.notify();
+      dep.notify(key);
     },
   });
 }
+
+/**
+ *
+ *
+ * @class watcher
+ */
 class watcher {
-  constructor(vm, data, root) {
+  constructor(vm, data, node) {
     this.depIds = {};
     this.vm = vm;
     this.data = data;
-    this.root = root;
+    this.node = node;
     this.val = this.get();
   }
   addDep(dep) {
@@ -65,8 +95,8 @@ class watcher {
     const val = this.get();
     if (val !== this.val) {
       this.val = val;
-      compile(this.vm.$options.data, this.root);
-      console.log('重新compile');
+      compile(this.vm.$options.data, this.node);
+      // console.log('重新compile');
     }
   }
   get() {
@@ -77,58 +107,77 @@ class watcher {
   }
 }
 const reg = /^\{{2}\S+\}{2}/;
-class Vue {
+
+/**
+ *
+ *
+ * @class Template
+ */
+class Template {
   constructor(options = {}) {
     this.$options = options;
-    const data = this.$options.data;
+    this.$data = this.$options.data;
+    observe(this.$data);
+    this.seperate();
+  }
+  seperate() {
     const root = document.querySelector(this.$options.root);
-    const node = createNode(root, reg);
-    observe(data);
-    // 第一次渲染
-    compile(data, node);
-    Object.keys(data).forEach(key => {
-      this._proxy(key);
-      // watch 到变更信息
-      this.$Watch(key, root);
-      // 重新渲染
-      // compile(data, node);
-    });
-
+    if (root) {
+      const node = createNode(root, reg);
+      compile(this.$data, node);
+      Object.keys(this.$data).forEach(key => {
+        this.$Watch(key, node);
+      });
+    }
   }
-  $Watch(data, root) {
-    new watcher(this, data, root);
-  }
-  _proxy(key) {
-    Object.defineProperty(this, key, {
-      get: () => this.$options.data[key],
-      set: val => {
-        this.$options.data[key] = val;
-      },
-    });
+  $Watch(data, node) {
+    new watcher(this, data, node);
   }
 }
 // 需要把dom 上的节点拿到并且生成
+
+/**
+ *渲染dom 的节点，
+ *
+ * @class dom
+ */
 class dom {
   constructor(data, node) {
     this.getNode(data, node);
   }
   getNode(data, node) {
-    // console.log('compile', data);
     Object.keys(node).forEach(key => {
+      // 为了使HTML里面的字段名{{name}}跟templat里面的data相匹配，在第一次渲染时做了前后的字符串处理。
       let str = node[key].innerHTML.substr(2);
       str = str.substr(0, str.length - 2);
+      const attr = node[key].getAttribute('data_attr');
       for (const prop in data) {
-        if (str === prop) {
-          node[key].innerHTML = data[prop];
+        // 在不存在attr值并且，html的字段名跟template的属性名相匹配的时候进入此循环
+        if ((!attr) && str === prop) {
+          // 第一次渲染发生时，给当前key 加上当前渲染的data_attr,为了后面再次更新做匹配
+          node[key].setAttribute('data_attr', str);
+          // 赋值
+          node[key].innerText = data[prop];
+        } else if (prop === attr) {
+          // 当后续渲染的attr值跟data 的prop 值相同的情况下，才进行一个重新渲染。
+          node[key].innerText = data[prop];
         }
       }
     });
   }
 }
 
+/**
+ *
+ *
+ * @param {*} data
+ * @param {*} node
+ * @returns
+ */
 function compile(data, node) {
   return new dom(data, node);
 }
+
 function createNode(root, reg) {
 
   const nodeIterator = document.createNodeIterator(
@@ -148,29 +197,35 @@ function createNode(root, reg) {
   }
   return pars;
 }
-// const root = document.querySelector('#app');
 
-
-// const p = document.getElementById('p');
-// const input = document.getElementById('input');
-// const hello = document.getElementById('test');
-// input.addEventListener('keyup', function(e) {
-//   demo1.text = e.target.value;
-// });
-// demo1.$Watch('text', str => (p.innerHTML = str));
-// document.addEventListener('DOMContentLoaded', function() {
-//   demo2.$Watch('text', str => (hello.innerHTML = str));
-//   setTimeout(() => {
-//     demo2.$options.data.text = 'it\'s changed...';
-//   }, 3000);
-// }, false);
 document.addEventListener('DOMContentLoaded', function() {
-  const opt = { root: '#app', data: { name: '检索中...', age: 30 } };
-  const vm = new Vue(opt);
+  const opt = {
+    root: '#app',
+    data: {
+      name: '检索中...',
+      age: 30,
+    },
+  };
+  const vm = new Template(opt);
   // opt.data.name = 'dsdssd';
   setTimeout(() => {
     opt.data.name = '王永峰';
     opt.data.age = 89;
-    // console.log(opt.data.name);
   }, 2000);
 }, false);
+
+const input = document.getElementById('input');
+const demo = new Template({
+  root: '#name',
+  data: {
+    text: '',
+    name: '',
+  },
+});
+input.addEventListener('keyup', function(e) {
+  demo.$options.data.text = e.target.value;
+});
+setTimeout(() => {
+  demo.$options.data.name = '122';
+  console.log(demo.$options.data);
+}, 2000);
